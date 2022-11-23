b64d759d75cc37ecd62a6ccddcf74c4bd1ca6bb8
Vaishnavi Ravindra Deshmukh <vdeshmukh@cemtrexlabs.com>
2022-11-23 15:26:49 +0530

    Day3

--- resources/item.py
+++ resources/item.py
@@ -2,53 +2,57 @@ from flask import request
 import uuid
 from db import items
 from flask.views import MethodView
-from flask_smorest import Blueprint
-from schema import ItemSchema
+from flask_smorest import Blueprint,abort
+from schema import ItemSchema, ItemGetSchema,SuccessMessageSchema,ItemOptionalQuerySchema,ItemQuerySchema
 
 blp=Blueprint("items",__name__,description="Operations GET, PUT, POST, DELETE")
 
 @blp.route("/item")
 class Item(MethodView):
-    def get(self):
-        id= request.args.get('id')
+
+    @blp.response(200,ItemGetSchema(many=True)) 
+    @blp.arguments(ItemOptionalQuerySchema, location="query")
+    def get(self,args):
+        id= args.get("id")
         if id is None:
             return items
-        try:
-            for item in items:
-                if item['id']==id:
-                    return item
-        except KeyError:
-            return {"message":"Item not found"}, 404
+        for item in items:
+            if item["id"]==id:
+                return [item]
+        abort(400, message="Id not found")
 
     @blp.arguments(ItemSchema)
-    def put(self, request_data):
-        # request_data=request.get_json()
-        id= request.args.get('id')
-        if id== None:
-            return {"message":"Id not given"}, 404
-        try:
-            for item in items:
-                if item['id']==id:
-                    item['item']['name']=request_data['name']
-                    item['item']['price']=request_data['price']
-                    return {"message":"Item updated succesfully"},200
-        except KeyError:
-            return {"message":"Item not found"}, 404
+    @blp.response(200,SuccessMessageSchema)
+    @blp.arguments(ItemQuerySchema, location="query")
+    def put(self, request_data,args):
+        id= args.get("id")
+        for item in items:
+            if item["id"]==id:
+                item["item"]["name"]=request_data["name"]
+                item["item"]["price"]=request_data["price"]
+                return {"message":"Item updated succesfully"},200
+        abort(400, message="Id not found")
 
 
     @blp.arguments(ItemSchema)
+    @blp.response(201,SuccessMessageSchema)
     def post(self,request_data):
-        # request_data=request.get_json()
-        # if 'name' not in request_data or 'price' not in request_data:
-        #     return {"message":"'name' and 'price' must be in body"}, 400
-        items[uuid.uuid4().hex]=request_data
+        item={
+            "id":uuid.uuid4().hex,
+            "item":{
+                "name":request_data["name"],
+                "price":request_data["price"]
+            }
+        }
+        items.append(item)
         return {"message":"Item added succesfully"},201
 
-    def delete(self):
-        id= request.args.get('id')
-        if id== None:
-            return {"message":"Id not given"}, 404
-        if id in items.keys():
-            del items[id]
-            return {"message":"Item deleted succesfully"},200
-        return {"message":"Item not found"}, 404
+    @blp.arguments(ItemQuerySchema, location="query")
+    @blp.response(200,SuccessMessageSchema)
+    def delete(self,args):
+        id= args.get("id")
+        for item in items:
+            if item["id"]==id:
+                items.remove(item)
+                return {"message":"Item deleted succesfully"},200
+        abort(400, message="Id not found")
--- schema.py
+++ schema.py
@@ -3,3 +3,16 @@ from marshmallow import Schema, fields
 class ItemSchema(Schema):
     name=fields.Str(required=True)
     price=fields.Int(required=True)
+
+class ItemGetSchema(Schema):
+    id=fields.Str(dump_only=True)
+    item=fields.Nested(ItemSchema)
+
+class SuccessMessageSchema(Schema):
+    message=fields.Str(dump_only=True)
+
+class ItemQuerySchema(Schema):
+    id=fields.Str(required=True)
+
+class ItemOptionalQuerySchema(Schema):
+    id=fields.Str(required=False)
