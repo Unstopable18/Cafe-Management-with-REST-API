d1ea5e349eebb887685386991a930a0d9f1d76c5
Vaishnavi Ravindra Deshmukh <vdeshmukh@cemtrexlabs.com>
2022-11-28 18:27:51 +0530

    Day 5 models started

--- CafeManagement/cafesite/cafesite/settings.py
+++ CafeManagement/cafesite/cafesite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cafeManagement'
 ]
 
 MIDDLEWARE = [
@@ -72,11 +73,15 @@ WSGI_APPLICATION = 'cafesite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+#vaishnavi@127.0.0.1:3306
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+        'NAME': 'cafe',
+        'USER':'vaishnavi',
+        'PASSWORD':'8806812990',
     }
 }
 
